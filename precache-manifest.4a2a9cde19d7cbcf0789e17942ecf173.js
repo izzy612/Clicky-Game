a4a4961d624b37132522236adf69bed721d2cf77
Isabella Torres <izzy612@gmail.com>
2019-05-30 12:19:15 -0400

    Updates

---
+++ precache-manifest.4a2a9cde19d7cbcf0789e17942ecf173.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1521e398e518c2e4bea9c5c5ff678ef6",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "8e1d9fbdb8efed63e97c",
+    "url": "/Clicky-Game/static/css/main.580c1fa8.chunk.css"
+  },
+  {
+    "revision": "86216672019027bf6391",
+    "url": "/Clicky-Game/static/js/2.3052869d.chunk.js"
+  },
+  {
+    "revision": "8e1d9fbdb8efed63e97c",
+    "url": "/Clicky-Game/static/js/main.a426f8d7.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
